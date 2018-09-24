eb26c48fb6933154cdb4db8906db4156fbb4c794
torte007 <torte007@fiu.edu>
2018-09-23 22:06:10 -0400

    second commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.css';
+import Book from './components/book';
+
+ReactDOM.render(<Book />, document.getElementById("root"));
