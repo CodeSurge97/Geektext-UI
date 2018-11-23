1179b98ff9e9f812c03bd3bef668bdf5a53b14b6
Nick Nunez <nnune028@fiu.edu>
2018-11-23 16:34:33 -0500

    Changed timeout to 1500

--- src/components/Comment/addComment.jsx
+++ src/components/Comment/addComment.jsx
@@ -54,7 +54,7 @@ class AddComment extends Component {
           })
         })
         console.log("Contents: " + this.state.text)
-        window.setTimeout(()=> (window.location = "http://geek.localhost.com:3000/book/" + this.state.isbn), 1000);
+        window.setTimeout(()=> (window.location = "http://geek.localhost.com:3000/book/" + this.state.isbn), 1500);
     }
     updateCharacters(length) {
         this.setState({
